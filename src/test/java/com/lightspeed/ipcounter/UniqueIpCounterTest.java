ea24e1f300b6f84a7048f3b0256911f03a64d40f
Vardan Mkrtchyan <vardan.mkrtchyan@wmg.com>
2024-08-10 04:41:58 +0400

    Change total count of IP's in test file to 100 for quick run and update expected value in test.

--- src/test/java/com/lightspeed/ipcounter/UniqueIpCounterTest.java
+++ src/test/java/com/lightspeed/ipcounter/UniqueIpCounterTest.java
@@ -9,7 +9,7 @@ public class UniqueIpCounterTest {
   @Test
   public void testCount() {
     final String file = "src/test/resources/ipv4_addresses.txt";
-    final long expectedCount = 700L;
+    final long expectedCount = 70L;
     final long actualCount = UniqueIpCounter.countUniques(file);
     assertEquals(expectedCount, actualCount);
   }
